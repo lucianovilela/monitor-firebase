ac658682d3945a4bd204c01c26e985c01246e145
Luciano Vilela Dourado <luciano.dourado@mpt.mp.br>
2016-08-01 19:33:49 -0300

    Versao Funcionando

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,36 +11,38 @@ angular.module('starter.controllers', ['firebase', 'googlechart'])
 })
 .controller('PlaylistCtrl', function($scope, $stateParams, $firebaseArray, $firebaseObject) {
   var itemsRef =  firebase.database().ref('/monitor/commands/'+$stateParams.idCommand);
-  var command = $firebaseObject(itemsRef);
+  
+  
+  $scope.command = $firebaseObject(itemsRef);
 
-  itemsRef =  firebase.database().ref('/monitor/database/'+$stateParams.idCommand+'/data').limitToLast(50);
-  var dados = $firebaseArray(itemsRef);
-  $scope.chartObject = {};
+  var dataRef =  firebase.database().ref('/monitor/database/'+$stateParams.idCommand+'/data').limitToLast(50);
 
-  $scope.chartObject.type = "AreaChart";
-  var d = [];
-  dados.forEach(function(data){
-    s.push(
-      {
-          c: [
-             { v: data.time },
-             { v: data.value }
-          ]
-      }
 
-    );
+  dataRef.on('value', function(snapshot){
+	  var d = [];
+	  snapshot.forEach(function(value){
+			
+			d.push({c: [
+             { v: new Date(value.val().time) },
+             { v: value.val().value }
+			]});		  
+	  });
+	  $scope.chartObject = {};
+	  $scope.chartObject.type = "LineChart";
+	   $scope.chartObject.options = {
+			'title': 'teste',
+			'hAxis':{'formatType' : 'long'}
+		};
+	  	  
+	  $scope.chartObject.data = {
+		   "cols": [
+			   { id: "t", label: "Date", type: "datetime" },
+			   { id: "s", label: "Value", type: "number" }
+		   ],
+		   "rows": d
+	   };
+
   });
 
-  $scope.chartObject.data = {
-       "cols": [
-           { id: "t", label: "Date", type: "string" },
-           { id: "s", label: "Value", type: "number" }
-       ],
-       "rows": d
-   };
-
-   $scope.chartObject.options = {
-       'title': command.name
-   };
 
 });
