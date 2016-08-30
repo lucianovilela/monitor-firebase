2781ad7178c26ae5f97e6daf54488c7d0273d1fe
Luciano Vilela <lucianovilela@gmail.com>
2016-08-30 15:52:11 -0300

    versao com tabs

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,35 +12,46 @@ angular.module('starter.controllers', ['firebase', 'googlechart'])
 })
 .controller('PlaylistCtrl', function($scope, $stateParams, $firebaseArray, $firebaseObject) {
 //	window.ga('send', { hitType: 'pageview', page: "/monitor/graph"});
-  var itemsRef =  firebase.database().ref('/monitor/commands/'+$stateParams.idCommand);
-
-
-  $scope.command = $firebaseObject(itemsRef);
-
-  var dataRef =  firebase.database()
-	    .ref('/monitor/database/'+$stateParams.idCommand+'/data').limitToLast(10);
   $scope.chartObject = {};
-  $scope.d = [];
-  $scope.chartObject.type = "LineChart";
   $scope.chartObject.options = {
-		'title': $scope.command.name,
-		'hAxis':{'formatType' : 'long'}
+			'title': '',
+			'hAxis':{'formatType' : 'long'}
    };
 
-    $scope.chartObject.data = {
+  $scope.chartObject.data = {
 		   "cols": [
 			   { id: "t", label: "Date", type: "datetime" },
 			   { id: "s", label: "Value", type: "number" }
 		   ],
 		   "rows": []
-	};
+	};  
+  
+  $scope.chartObject.type = "LineChart";
+  var itemsRef =  firebase.database().ref('/monitor/commands/'+$stateParams.idCommand);
+
+  itemsRef.once('value', function(snapshot){
+	$scope.chartObject.options = {
+			'title': snapshot.val().name,
+			'hAxis':{'formatType' : 'long'}
+	   };
+
+	  
+  });
+  
+  $scope.command = $firebaseObject(itemsRef);
+
+  var dataRef =  firebase.database()
+	    .ref('/monitor/database/'+$stateParams.idCommand+'/data').limitToLast(10);
+
+  
+
 
   dataRef.on('value', function(snapshot){
       $scope.atualizado = new Date();
 			$scope.chartObject.data.rows=[];
-	  console.log(new Date()+"atualizacao");
+
 	  snapshot.forEach(function(value){
-			console.log(new Date(value.val().time) + " "+value.val().value);
+
 		$scope.chartObject.data.rows.push({c: [
              { v: new Date(value.val().time) },
              { v: value.val().value }
